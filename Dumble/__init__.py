df67cdc6c24aa35e1c3aeba800dad645aca774d9
Vrunda Patel <vrunda.p@ahduni.edu.in>
2023-07-14 19:24:58 +0530

    login_Validation

--- Dumble/__init__.py
+++ Dumble/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
+from flask_bcrypt import Bcrypt
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
-
 db=SQLAlchemy(app)
 app.app_context().push()
 
 
 app.config['SECRET_KEY']='95e6991c00be2df05c6671fd'
 #secret key is required for post method beacuse we are submiting form 
+
+
+#hash password not number 
+bcrypt=Bcrypt(app)
 
 from Dumble import routes
