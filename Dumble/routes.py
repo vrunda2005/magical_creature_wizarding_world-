f3d503c0910101ecbe5d13cff194b48caf5685e8
Vrunda Patel <vrunda.p@ahduni.edu.in>
2023-07-16 22:55:21 +0530

    changed little

--- Dumble/routes.py
+++ Dumble/routes.py
@@ -2,8 +2,8 @@ from Dumble import app
 from Dumble import db 
 from flask import render_template,redirect,url_for,flash,get_flashed_messages
 from Dumble.model import UserInfo
-from Dumble.forms import RegisterForm
-
+from Dumble.forms import RegisterForm,LoginForm
+from flask_login import login_user
 
 
 @app.route("/")
@@ -14,10 +14,6 @@ def index():
 def creatures_page():
     return render_template("creatures.html")
 
-
-@app.route("/login")
-def login_page():
-    return render_template("login.html")
 
 @app.route("/register", methods=['GET','POST'])
 def register_page():
@@ -33,5 +29,22 @@ def register_page():
         for err_msg in form.errors.values():
             flash(f'ERROR{err_msg}',category='danger')
             
-
     return render_template('register.html',forms=form)
+
+@app.route("/login",methods=['GET','POST'])
+def login_page():
+    form=LoginForm()
+    if form.validate_on_submit():  #work when click submit  or form is validate
+        #checking var with username 
+        attempted_user=UserInfo.query.get(form.username.data).first()
+        #if none 
+        if attempted_user and attempted_user.check_password_correction(
+            attemted_password=form.password.data):
+
+                login_user(attempted_user)
+                flash('Success! you are logged in {{attempted_user.name}}',category='success')
+                return redirect(url_for('creatures_page'))
+
+        else:
+            flash('username and password not match try another ',category='danger')
+    return render_template('login.html',forms=form)
