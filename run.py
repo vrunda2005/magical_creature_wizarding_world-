c99c3a53c5a252151ccee8bc08bebad43ceb7b8d
Vrunda Patel <vrunda.p@ahduni.edu.in>
2023-08-07 20:53:20 +0530

    test 3

--- run.py
+++ run.py
@@ -4,6 +4,10 @@
 from Dumble import app,db
 
 
+# if __name__=="__main__":
+#     db.create_all()
+#     app.run(debug=True,host="0.0.0.0",port=500)
+
 if __name__=="__main__":
     db.create_all()
-    app.run(debug=True,host="0.0.0.0",port=500)
+    app.run(debug=True,port=500)
